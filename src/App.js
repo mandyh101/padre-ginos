8176086f2a6399ca89d89c9dcfa35e3edf5508d4
mandy <mandyhale10@gmail.com>
2025-03-11 17:38:57 +1300

    build a no frillsvanilla react component for pizza

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+
+const Pizza = (props) => {
+  return React.createElement(
+    "div", 
+    {}, 
+    [
+      React.createElement(
+      "h1", 
+      {}, 
+      props.name
+    ), React.createElement(
+      "p",
+      {},
+      props.description
+    )
+  ]
+  )
+}
 const App = () => {
   return React.createElement(
     "div", //tag
     {}, //attributes
-    React.createElement(
-      "h1", //tag
-      {}, //element
-      "Padre Gino's" //content
-    )
+    [
+      React.createElement(
+        "h2", //tag
+        {}, //element
+        "Padre Gino's" //content
+      ),
+      React.createElement(Pizza, {name: "Pepperoni", description: "Pepperoni"}), //component
+      React.createElement(Pizza, {name: "Hawaiian", description: "Pineapple, Ham, Good vibes"}), //component
+      React.createElement(Pizza, {name: "Margherita", description: "Tomato, Basil, Mozzarella"}), //component
+    ]
   )
 }
 
