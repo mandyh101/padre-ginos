1c9c6b82cf31651aa1c1396b987d6935e8cfb63f
mandy <mandyhale10@gmail.com>
2025-03-11 17:55:08 +1300

    config prettier formatting with defaults

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-
 const Pizza = (props) => {
-  return React.createElement(
-    "div", 
-    {}, 
-    [
-      React.createElement(
-      "h1", 
-      {}, 
-      props.name
-    ), React.createElement(
-      "p",
-      {},
-      props.description
-    )
-  ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 const App = () => {
   return React.createElement(
     "div", //tag
@@ -24,15 +12,24 @@ const App = () => {
       React.createElement(
         "h2", //tag
         {}, //element
-        "Padre Gino's" //content
+        "Padre Gino's", //content
       ),
-      React.createElement(Pizza, {name: "Pepperoni", description: "Pepperoni"}), //component
-      React.createElement(Pizza, {name: "Hawaiian", description: "Pineapple, Ham, Good vibes"}), //component
-      React.createElement(Pizza, {name: "Margherita", description: "Tomato, Basil, Mozzarella"}), //component
-    ]
-  )
-}
+      React.createElement(Pizza, {
+        name: "Pepperoni",
+        description: "Pepperoni",
+      }),
+      React.createElement(Pizza, {
+        name: "Hawaiian",
+        description: "Pineapple, Ham, Good vibes",
+      }),
+      React.createElement(Pizza, {
+        name: "Margherita",
+        description: "Tomato, Basil, Mozzarella",
+      }),
+    ],
+  );
+};
 
-const container = document.getElementById("root") //first get the cdiv to render the app in
-const root = ReactDOM.createRoot(container) //set a root variable using reactDOM
-root.render(React.createElement(App)) //render the app to the root
+const container = document.getElementById("root"); //first get the cdiv to render the app in
+const root = ReactDOM.createRoot(container); //set a root variable using reactDOM
+root.render(React.createElement(App)); //render the app to the root
