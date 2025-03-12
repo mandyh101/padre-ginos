be6e85dd32fe1633640d79d81dc90d30a7566560
mandy <mandyhale10@gmail.com>
2025-03-13 07:47:56 +1300

    useEffect hook to fetch pizzas for selecting from api

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,14 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Pizza from "./pizza";
+
+const intl = new Intl.NumberFormat("en-NZ", {
+  style: "currency",
+  currency: "NZD",
+});
 
 //* Named functions show up in the stack trace which can be helpful for debugging. Arrow functions as anon functions won't show up. Not super important but interesting!
 //* two way binding is not free in react - enter useState hook to set and update component state
+//* the useEffect hook is used to do something (an effect) as soon as a component is rendered: component renders, effect runs
 export default function Order() {
   // const pizzaType = "pepperoni ";
   // const pizzaSize = "M";
-
   const [pizzaType, setPizzaType] = useState("pepperoni");
   const [pizzaSize, setPizzaSize] = useState("M  ");
+  const [pizzaTypes, setPizzaTypes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  let price, selectedPizza; //immutable, selectedPizza is always pizzaType from PizzaTypes, and price is always from selectedPizza
+
+  if (!loading) {
+    selectedPizza = pizzaTypes.find((pizza) => pizzaType === pizza.id);
+  }
+  /**
+   * ! this is the side effect function
+   * Fetches the list of pizza types from the API.
+   * Updates the component state with the fetched data.
+   * Sets the loading state to false once the data is retrieved.
+   */
+  async function fetchPizzaTypes() {
+    const pizzaRes = await fetch("/api/pizzas");
+    const pizzaJson = await pizzaRes.json();
+    setPizzaTypes(pizzaJson);
+    setLoading(false);
+  }
+  //* use useEffect to run our fecth function outside the render cycle so the function doesn't keep running everytime the component is rendered, ou only want it to run once at the start of mounting th component
+  useEffect(() => {
+    fetchPizzaTypes();
+  }, []); //empty array means no variables tracked so this only ever runs once when the component is rendered, its not watching any variables
+
   return (
     <div className="order">
       <h2>Create Order</h2>
@@ -21,9 +51,11 @@ export default function Order() {
               value={pizzaType}
               onChange={(e) => setPizzaType(e.target.value)}
             >
-              <option value="pepperoni">The Pepperoni Pizza</option>
-              <option value="hawaiian">The Hawaiian Pizza</option>
-              <option value="big_meat">The Big Meat Pizza</option>
+              {pizzaTypes.map((pizza) => (
+                <option key={pizza.id} value={pizza.name}>
+                  {pizza.name}
+                </option>
+              ))}
             </select>
           </div>
           <div>
