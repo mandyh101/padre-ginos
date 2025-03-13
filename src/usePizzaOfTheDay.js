848fc18c96c707667fa5c0fc12c59e40ad4cd893
mandy <mandyhale10@gmail.com>
2025-03-13 15:47:20 +1300

    learning notes: custom hooks and hooks

--- src/usePizzaOfTheDay.js
+++ src/usePizzaOfTheDay.js
@@ -1,6 +1,7 @@
 //* the function behind a custom hook is it's just a function that can call other functions
-//* use this to create common functionality that can be used across different components
 //* you know it's a hook if it starts with use
+//* create a custom hook if an effect needs to be shared or tested e.g. here the pizza of the day will be imported in differnt pages in the app - hard sell!
+//* a rule on hooks (applies to custom hooks and all hooks) - never place them inside conditionals as this will create unexpected beahviours.
 
 import { useState, useEffect } from "react";
 
