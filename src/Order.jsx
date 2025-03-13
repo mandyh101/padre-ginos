b5231ed086370da4a7e3aa7b997ea9bb156db094
mandy <mandyhale10@gmail.com>
2025-03-14 11:03:38 +1300

    feat: add cart component and submit pizzas to the cart

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Pizza from "./pizza";
+import Cart from "./Cart";
 
 const intl = new Intl.NumberFormat("en-NZ", {
   style: "currency",
@@ -14,6 +15,7 @@ export default function Order() {
   const [pizzaSize, setPizzaSize] = useState("M");
   const [pizzaTypes, setPizzaTypes] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [cart, setCart] = useState([]);
 
   let price, selectedPizza; //immutable, selectedPizza is always pizzaType from PizzaTypes, and price is always from selectedPizza
 
@@ -23,7 +25,6 @@ export default function Order() {
       selectedPizza.sizes ? selectedPizza.sizes[pizzaSize] : "",
     );
   }
-  console.log(price, pizzaSize);
 
   /**
    * ! this is the side effect function
@@ -43,82 +44,92 @@ export default function Order() {
     fetchPizzaTypes();
   }, []); //empty array means no variables tracked so this only ever runs once when the component is rendered, its not watching any variables
 
-  console.log(selectedPizza);
   return (
-    <div className="order" style={{ marginLeft: "0" }}>
-      <h2>Create Order</h2>
-      <form>
-        <div>
+    <div className="order-page">
+      <div className="order">
+        <h2>Create Order</h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setCart([
+              ...cart,
+              { pizza: selectedPizza, size: pizzaSize, price },
+            ]);
+          }}
+        >
           <div>
-            <label htmlFor="pizza-type">Pizza Type</label>
-            <select
-              name="pizza-type"
-              value={pizzaType}
-              onChange={(e) => setPizzaType(e.target.value)}
-            >
-              {/*interesting note on using key, tying key tot he index is not stable and is not good practice as it is not actually related to the object being mapped like an id is for example. So use a related value on the object, or if it doesn't matter if data gets reorganised you can use index with all this in mind! */}
-              {pizzaTypes.map((pizza) => (
-                <option key={pizza.id} value={pizza.id}>
-                  {pizza.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="pizza-size">Pizza Size</label>
             <div>
-              <span>
-                <input
-                  checked={pizzaSize === "S"}
-                  type="radio"
-                  name="pizza-size"
-                  value="S"
-                  id="pizza-s"
-                  onChange={(e) => setPizzaSize(e.target.value)}
-                />
-                <label htmlFor="pizza-s">Small</label>
-              </span>
-              <span>
-                <input
-                  checked={pizzaSize === "M"}
-                  type="radio"
-                  name="pizza-size"
-                  value="M"
-                  id="pizza-m"
-                  onChange={(e) => setPizzaSize(e.target.value)}
-                />
-                <label htmlFor="pizza-m">Medium</label>
-              </span>
-              <span>
-                <input
-                  checked={pizzaSize === "L"}
-                  type="radio"
-                  name="pizza-size"
-                  value="L"
-                  id="pizza-l"
-                  onChange={(e) => setPizzaSize(e.target.value)}
-                />
-                <label htmlFor="pizza-l">Large</label>
-              </span>
+              <label htmlFor="pizza-type">Pizza Type</label>
+              <select
+                name="pizza-type"
+                value={pizzaType}
+                onChange={(e) => setPizzaType(e.target.value)}
+              >
+                {/*interesting note on using key, tying key tot he index is not stable and is not good practice as it is not actually related to the object being mapped like an id is for example. So use a related value on the object, or if it doesn't matter if data gets reorganised you can use index with all this in mind! */}
+                {pizzaTypes.map((pizza) => (
+                  <option key={pizza.id} value={pizza.id}>
+                    {pizza.name}
+                  </option>
+                ))}
+              </select>
             </div>
+            <div>
+              <label htmlFor="pizza-size">Pizza Size</label>
+              <div>
+                <span>
+                  <input
+                    checked={pizzaSize === "S"}
+                    type="radio"
+                    name="pizza-size"
+                    value="S"
+                    id="pizza-s"
+                    onChange={(e) => setPizzaSize(e.target.value)}
+                  />
+                  <label htmlFor="pizza-s">Small</label>
+                </span>
+                <span>
+                  <input
+                    checked={pizzaSize === "M"}
+                    type="radio"
+                    name="pizza-size"
+                    value="M"
+                    id="pizza-m"
+                    onChange={(e) => setPizzaSize(e.target.value)}
+                  />
+                  <label htmlFor="pizza-m">Medium</label>
+                </span>
+                <span>
+                  <input
+                    checked={pizzaSize === "L"}
+                    type="radio"
+                    name="pizza-size"
+                    value="L"
+                    id="pizza-l"
+                    onChange={(e) => setPizzaSize(e.target.value)}
+                  />
+                  <label htmlFor="pizza-l">Large</label>
+                </span>
+              </div>
+            </div>
+            <button type="submit">Add to Cart</button>
           </div>
-          <button type="submit">Add to Cart</button>
-        </div>
-        <div className="order-pizza">
-          {loading ? (
-            <h2>Loading pizzas...</h2>
-          ) : (
-            <>
-              <Pizza
-                name={selectedPizza.name}
-                description={selectedPizza.description}
-                image={selectedPizza.image}
-              />
-              <p>{price}</p>
-            </>
-          )}
-        </div>
-      </form>
+          <div className="order-pizza">
+            {loading ? (
+              <h2>Loading pizzas...</h2>
+            ) : (
+              <>
+                <Pizza
+                  name={selectedPizza.name}
+                  description={selectedPizza.description}
+                  image={selectedPizza.image}
+                />
+                <p>{price}</p>
+              </>
+            )}
+          </div>
+        </form>
+      </div>
+      {loading ? <h2>LOADING …</h2> : <Cart cart={cart} />}
     </div>
   );
 }
