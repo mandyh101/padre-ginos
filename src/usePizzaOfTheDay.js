b1d4832a25a8c3a1e7af7ee94bd0c6312ea1a973
mandy <mandyhale10@gmail.com>
2025-03-14 10:44:32 +1300

    debugging: example using usDebugValue to debug custom hooks

--- src/usePizzaOfTheDay.js
+++ src/usePizzaOfTheDay.js
@@ -3,10 +3,17 @@
 //* create a custom hook if an effect needs to be shared or tested e.g. here the pizza of the day will be imported in differnt pages in the app - hard sell!
 //* a rule on hooks (applies to custom hooks and all hooks) - never place them inside conditionals as this will create unexpected beahviours.
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  //useDebugValue hook for debugging custom hooks at a glance in react dev tools
+  useDebugValue(
+    pizzaOfTheDay
+      ? `debug value: ${pizzaOfTheDay.id} : ${pizzaOfTheDay.name}`
+      : "loading",
+  );
 
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
