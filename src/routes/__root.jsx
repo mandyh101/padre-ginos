0a97b694c4711f6a356525e607ce132137d94c34
mandy <mandyhale10@gmail.com>
2025-03-14 12:19:09 +1300

    set up root route and refactor App.js to use tanstack RouterProvider

---
+++ src/routes/__root.jsx
@@ -0,0 +1,30 @@
+// __root.jsx = what code does every route in the app share
+
+import { useState } from "react";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import PizzaOfTheDay from "../PizzaOfTheDay";
+import Header from "../Header";
+import { CartContext } from "../contexts";
+
+export const Route = createRootRoute({
+  //give the root route what to expect:
+  component: () => {
+    const cartHook = useState([]);
+    //NOTE: we use Provider on the end of our context to pass the value of our context to every component wrapped inside this tag. here, our whole app is wrapped in CartContext so every component will have access. If we only wanted the Order and PizzaOfTheDay to access the context we would only wrap those components */}
+    //NOTE: in React 19 you can drop Provider! */}
+    return (
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            {/* outlet is where the pages/components that are dynamic will be swapped out by each child route */}
+            <Outlet />
+            <PizzaOfTheDay />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
+});
