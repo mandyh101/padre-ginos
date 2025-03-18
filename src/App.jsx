a7b89ed24db21eaff9d61c27dac6240a674ffabd
mandy <mandyhale10@gmail.com>
2025-03-18 13:54:20 +1300

    feat: add tanstack query to project to fetch past orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 
 const router = createRouter({ routeTree });
-
+const queryClient = new QueryClient();
 const App = () => {
   return (
     //* wrapping an app in StrictMode checks and gives you additional warnings about things you shouldn't be doing and also warns you if you are using features or packags that will be deprecated!
     <StrictMode>
+      <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      </QueryClientProvider>
     </StrictMode>
   );
 };
