cf808817bf1256b16c159be9f7d101a158a531ba
mandy <mandyhale10@gmail.com>
2025-03-18 13:53:45 +1300

    feat: link to home page in header logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
 
   return (
     <nav>
-      <Link>
+      <Link to={"/"}>
         <h1 className="logo">Padre Gino's Pizza</h1>
       </Link>
       <div className="nav-cart">
