afe67c054247e5bb7d11f19cbee42b44af97e2d6
mandy <mandyhale10@gmail.com>
2025-03-18 17:52:20 +1300

    feat: contact page and uncontrolled form with useMutation from tanstack query to post formData

---
+++ src/routes/contact.lazy.jsx
@@ -0,0 +1,38 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+import {useMutation} from '@tanstack/react-query'
+import postContact from '../api/postContact'
+
+export const Route = createLazyFileRoute('/contact')({
+  component: ContactRoute,
+})
+
+function ContactRoute() {
+  //this mutation is a result of the useMutation hook from query. It needs to be invoked to make the request
+  const mutation = useMutation({
+    mutationFn: function (e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      return postContact(
+        formData.get('name'),
+        formData.get('email'),
+        formData.get('message')
+      )
+    }
+  })
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      {mutation.isSuccess ? (
+        <h3>Submitted!</h3>
+      ) : (
+        //mutation.mutate is calling the mutation function that we created using the tansatck query useMutation hook
+        <form onSubmit={mutation.mutate}>
+          <input name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="Email" />
+          <textarea placeholder="Message" name="message"></textarea>
+          <button>Submit</button>
+        </form>
+      )}
+    </div>
+  )
+}
