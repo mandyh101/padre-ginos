955418fc56b93d9c9a2aa4f0b9c33cdeab7ade91
mandy <mandyhale10@gmail.com>
2025-03-18 14:51:59 +1300

    feat: write query to fetch past order

--- src/routes/past.lazy.jsx
+++ src/routes/past.lazy.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { createLazyFileRoute } from '@tanstack/react-router'
 import getPastOrders from '../api/getPastOrders'
+import getPastOrder from '../api/getPastOrder'
+import Modal from '../Modal'
+import {priceConverter} from '../useCurrency'
 
 export const Route = createLazyFileRoute('/past')({
   component: PastOrdersRoute,
@@ -9,10 +12,19 @@ export const Route = createLazyFileRoute('/past')({
 
 function PastOrdersRoute() {
   const [page, setPage] = useState(1)
+  const [focusedOrder, setFocusedOrder] = useState(null);
   const {isLoading, data} = useQuery({
     queryKey: ['past-orders', page],
     queryFn: () => getPastOrders(page),
     staleTime: 30000,
+  })
+
+  // note in this tanstack query we are renaming isLoading and data because we are using these named attributes already in anothe rquesry in the same file
+  const [isLoading: isLoadingPastOrder, data: pastOrderData] = useQuery({
+    queryKey: ['past-order', focusedOrder],
+    queryFn: () => getPastOrder(focusedOrder),
+    stale: 86400000, //one day in ms
+    enabled: !!focusedOrder //this query attribute allows us to only run the query if the focusedOrder is truthy
   })
 
   if (isLoading){
