8533ac7db4ef2bb76f0c9a72764307f4d40c7c23
mandy <mandyhale10@gmail.com>
2025-03-19 16:18:51 +1300

    test: config two seperate test workspaces - node for existing tests - browser for new tests using pw

---
+++ vitest.workspace.js
@@ -0,0 +1,27 @@
+import { defineWorkspace } from "vitest/config.js";
+
+export default defineWorkspace([
+  //this config keeps our tests we've created so far separated in a happy-dom environment
+  {
+    extends: "./vite.config.js",
+    test: {
+      include: ["**/*.node.test.{js,jsx}"],
+      name: "happy-dom",
+      environment: "happy-dom",
+    },
+  },
+  //this config creates a playwright environment where we will write some new browser tests using PW
+  {
+    extends: "./vite.config.js",
+    test: {
+      setupFiles: ["vitest-browser-react"],
+      include: ["**/*.browser.test.{js,jsx}"],
+      name: "browser",
+      browser: {
+        provider: "playwright",
+        enabled: true,
+        name: "chromium",
+      },
+    },
+  },
+]);
